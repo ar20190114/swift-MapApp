e0ed8ac1dea6c8d17aa50de2601ec7dc264f8bcd
ar20190114 <skdr0724@gmail.com>
2023-01-15 22:27:32 +0900

    create change view

--- MyMapApp/ContentView.swift
+++ MyMapApp/ContentView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     
     @State var inputText: String = ""
     @State var dispSearchKey: String = ""
+    @State var dispMapType: MKMapType = .standard
     
     var body: some View {
         VStack {
@@ -23,9 +25,41 @@ struct ContentView: View {
             }
             .padding()
             
-            MapView(searchKey: dispSearchKey)
+            ZStack(alignment: .bottomTrailing) {
+                // マップを表示
+                MapView(searchKey: dispSearchKey, mapType: dispMapType)
+                
+                // マップ種類切り替えボタン
+                Button(action: {
+                    // mapTypeプロパティー値をトグル
+                    // 標準　→ 航空写真　→ 航空写真＋標準
+                    // → 3D Flyover　→ 3D Flyover＋標準
+                    // → 交通機関
+                    if dispMapType == .standard {
+                        dispMapType = .satellite
+                    } else if dispMapType == .satellite {
+                        dispMapType = .hybrid
+                    } else if dispMapType == .hybrid {
+                        dispMapType = .satelliteFlyover
+                    } else if dispMapType == .satelliteFlyover {
+                        dispMapType = .hybridFlyover
+                    } else if dispMapType == .hybridFlyover {
+                        dispMapType = .mutedStandard
+                    } else {
+                        dispMapType = .standard
+                    }
+                }) {
+                    // マップアイコン表示
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0, height: 35.0, alignment: .leading)
+                } // Button ここまで
+                // 右の余白を20空ける
+                .padding(.trailing, 20.0)
+                // 下の余白を30空ける
+                .padding(.bottom, 30.0)
+            }
         }
-        .padding()
     }
 }
 
