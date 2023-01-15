95f53783db888ddc64454f67208f9366af57e2d2
ar20190114 <skdr0724@gmail.com>
2023-01-15 17:40:44 +0900

    Initial Commit

---
+++ MyMapApp/MyMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapAppApp.swift
+//  MyMapApp
+//
+//  Created by ryotaban on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct MyMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
